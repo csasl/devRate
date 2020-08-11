3588eb18102e794a50fa0af32c2a9c586d5e727c
Azra Lalji <azralalji1@gmail.com>
2020-08-11 06:04:21 +0000

    update condo routes, show page, enable adding new condos

--- models/condo.js
+++ models/condo.js
@@ -3,8 +3,11 @@ const mongoose = require("mongoose");
 const condoSchema = new mongoose.Schema({
 	name: String,
 	developer: String,
+	address: String,
+	postalCode: String,
 	image: String,
-	rent: String
+	rent: String,
+
 });
 
 module.exports = mongoose.model("Condo", condoSchema);
--- routes/condo.js
+++ routes/condo.js
@@ -2,13 +2,48 @@ const express = require("express"),
 	  router = express.Router(),
 	  Condo = require("../models/condo");
 
-//SHOW
+//INDEX
 router.get("/", (req, res)=>{
 	Condo.find({}, (err, allCondos)=>{
 		if(err){
 			console.log(err);
 		} else {
 			res.render("condos/index", {condos: allCondos});
+		}
+	});
+});
+
+//CREATE
+router.get("/new", (req, res)=>{
+	res.render("condos/new");
+});
+
+//NEW
+router.post("/", (req, res)=>{
+	let name = req.body.name,
+		  developer = req.body.developer,
+		  address = req.body.address,
+		  postalCode = req.body.postalCode,
+		  rent = req.body.rent,
+		  image = req.body.image,
+		  newCondo = {name: name, developer:developer, address: address, postalCode: postalCode, rent: rent, image:image};
+	//save condo to db
+	Condo.create(newCondo, (err, c)=>{
+		if(err){
+			console.log("ERROR");
+		} else {
+			res.redirect("/condos");
+		}
+	})
+});
+
+//SHOW
+router.get("/:id", (req, res)=>{
+	Condo.findById(req.params.id, (err, foundCondo)=>{
+		if(err){
+			res.redirect("/condos");
+		} else {
+			res.render("condos/show", {condo: foundCondo});
 		}
 	});
 });
