1019d33645a88e4829d3773d3288d68e0ac1c233
Azra Lalji <azralalji1@gmail.com>
2020-08-13 23:27:34 +0000

    add comment update and destroy routes, update show page

--- routes/comment.js
+++ routes/comment.js
@@ -1,11 +1,11 @@
 const express = require("express"),
 	  router = express.Router(),
 	  Condo = require("../models/condo"),
-	  Comment = require("../models/comment");
+	  Comment = require("../models/comment"),
+	  middleware = require("../middleware/index");
 
 //NEW COMMENT
-router.get("/condos/:id/comments/new", isLoggedIn, (req,res)=>{
-	console.log(req.params.id);
+router.get("/condos/:id/comments/new", middleware.isLoggedIn, (req,res)=>{
 	Condo.findById(req.params.id, (err, condo)=>{
 		if(err){
 			res.redirect("/condos");
@@ -16,7 +16,7 @@ router.get("/condos/:id/comments/new", isLoggedIn, (req,res)=>{
 });
 
 //CREATE COMMENT
-router.post("/condos/:id/comments", isLoggedIn, (req, res)=>{
+router.post("/condos/:id/comments", middleware.isLoggedIn, (req, res)=>{
 	Condo.findById(req.params.id, (err, foundCondo)=>{
 		if(err){
 			console.log(err);
@@ -36,18 +36,45 @@ router.post("/condos/:id/comments", isLoggedIn, (req, res)=>{
 					//ADD FLASH HERE
 					res.redirect("/condos/" + foundCondo._id);
 				}
-			})
+			});
 		}
 	});
-})
-//midlleware- check if logged findById
-function isLoggedIn(req, res, next) {
-	if(req.isAuthenticated()){
-		return next();
-	} else {
-		res.redirect("/login");
-	}
-}
+});
+
+//EDIT ROUTE
+router.get("/condos/:id/comments/:comment_id/edit", middleware.checkCommentOwnership, (req, res)=>{
+	Comment.findById(req.params.comment_id, (err, foundComment)=>{
+		if(err){
+			res.redirect("back");
+		} else {
+			res.render("comments/edit", {condo_id: req.params.id, comment:foundComment});
+		}
+	});
+});
+
+//COMMENT UPDATE ROUTE
+router.put("/condos/:id/comments/:comment_id", middleware.checkCommentOwnership, (req, res)=>{
+	Comment.findByIdAndUpdate(req.params.comment_id, req.body.comment, {new: true}, (err, updatedComment)=>{
+		if(err){
+			res.redirect("back");
+		} else {
+			//ADD FLASH HERE
+			res.redirect("/condos/" + req.params.id);
+		}
+	});
+});
+
+//COMMENT DESTROY ROUTE
+router.delete("/condos/:id/comments/:comment_id", middleware.checkCommentOwnership, (req, res)=>{
+	Comment.findByIdAndRemove(req.params.comment_id, (err)=>{
+		if(err){
+			res.redirect("/condos");
+		} else {
+			//ADD FLASH HERE
+			res.redirect("/condos/" + req.params.id);
+		}
+	});
+});
 
 module.exports = router;
 
